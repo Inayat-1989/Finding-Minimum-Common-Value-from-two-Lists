34d8827cb0886d7878078bf9e4d2ab9a4ec13e77
Inayat-1989 <inayat2291@gmail.com>
2026-05-19 12:35:06 +0500

    Two pointer approach of the same problem

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 list1 = list(map(int,input("Enter a list of values:").split()))
 list2 = list(map(int,input("Enter a list of values:").split()))
 list1, list2 = set(sorted(list1)), set(sorted(list2))
-intersection = list1 & list2
-if intersection:
-    print(min(intersection))
-else:
-    print(-1)
+i,j = 0,0
+for k in range(len(list1) + len(list2)):
+    if list1[i] == list2[j]:
+        print(list1[i])
+    elif list1[i] < list2[j]:
+        i = i + 1
+    elif list1[i] > list2[j]:
+        j = j + 1
+    if i == len(list1) or j == len(list2):
+        print(-1)
+print(-1)
